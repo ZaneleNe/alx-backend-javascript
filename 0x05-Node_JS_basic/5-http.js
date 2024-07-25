818234e8626fa589808f7e31717fb7848ac414f5
ZaneleNe <zanelenema97@gmail.com>
2024-07-25 12:19:51 +0000

    5-http.js

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -9,7 +9,7 @@ const DB_FILE = process.argv.length > 2 ? process.argv[2] : '';
 /**
  * Reads a CSV data file asynchronously.
  * @param {String} dataPath The path to the CSV data file.
- * @author Abdulkadir Badamasi <https://github.com/abmahuta>
+ * @author Zanele Nemaphohoni <https://github.com/zanelene>
  */
 const countStudents = (dataPath) => new Promise((resolve, reject) => {
   if (!dataPath) {
